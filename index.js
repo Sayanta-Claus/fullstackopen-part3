99f4dced1368c0baeb776a09d9e625d93a96dd69
Sayantan Biswas <sayantan251204@gmail.com>
2025-02-05 14:34:45 +0530

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+require('dotenv').config()
 const app=express()
 const morgan=require('morgan')
+const Person=require('./models/person')
 app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('body', function(req, res) {
@@ -9,28 +11,28 @@ morgan.token('body', function(req, res) {
     // we are using the host parameter
     app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": "1",
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": "2",
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": "3",
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": "4",
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 const daycodes=['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
 const months=['Jan','Feb','Mar','Apr','May','June','July','Aug','Sep','Oct','Nov','Dec']
@@ -44,26 +46,23 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/api/persons',(req,res)=>{
-    res.json(persons)
+    Person.find({}).then(result=>{
+            res.json(result)
+        })
 })
 
 app.get('/api/persons/:id',(req,res)=>{
     const id=req.params.id;
-    const person = persons.find((p)=>p.id===id)
-    if(person){
+    Person.findById(id).then(person=>{
         res.json(person)
-    }
-    else{
-        res.status(404).end()
-    }
+    })
 })
 
-app.delete('/api/persons/:id',(req,res)=>{
+app.delete('/api/persons/:id',(req,res,next)=>{
     const id=req.params.id;
-    persons=persons.filter((p)=>{
-        return p.id !== id
-    })
+   Person.findByIdAndDelete(id).then(result=>{
     res.status(204).end()
+   }).catch(err=>next(error))
 })
 
 app.post('/api/persons',(req,res)=>{
@@ -78,26 +77,57 @@ app.post('/api/persons',(req,res)=>{
             error:'number missing'
         })
     }
-    if (persons.find((p) => p.name === body.name)) {
-            return res.status(400).json({
-            error: 'Name must be unique'
-            });
-             }
-    const person= {
-        id:JSON.stringify(generateID()),
+    // if (persons.find((p) => p.name === body.name)) {
+    //         return res.status(400).json({
+    //         error: 'Name must be unique'
+    //         });
+    //          }
+    const person= new Person({
         name:body.name,
         number:body.number
+    })
+    person.save().then(newP=>{
+        res.json(newP)
+    })
+    
+})
+
+
+app.put('/api/persons/:id',(req,res,next)=>{
+    const id=req.params.id;
+    const body=req.body
+    const person={
+        name:body.name,
+        number:body.number,
     }
-    persons=persons.concat(person)
-    res.json(person)
+    Person.findByIdAndUpdate(id,person,{new:true}).then(updatedP=>{
+        res.json(updatedP)
+    }).catch(err=>next(err))
 })
 
 
-app.get('/info',(req,res)=>{
-   const date= new Date();
-    const string = `${daycodes[date.getDay()]} ${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} GMT${date.getTimezoneOffset()}`
-    res.send(`<p>Phonebook has info for ${persons.length} people</p> <br/> <p>${string}</p>`)
-})
+
+
+
+const errorHandler= (error,req,res,next)=>{
+    console.error(error.message)
+    if(error.name==='CastError'){
+        return res.status(400).send({
+            error:'malformatted id'
+        })
+    }
+    next(error)
+}
+app.use(errorHandler)
+
+
+
+
+// app.get('/info',(req,res)=>{
+//    const date= new Date();
+//     const string = `${daycodes[date.getDay()]} ${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} GMT${date.getTimezoneOffset()}`
+//     res.send(`<p>Phonebook has info for ${persons.length} people</p> <br/> <p>${string}</p>`)
+// })
 
 
 const PORT= process.env.PORT||3001
---
+++ mongo.js
@@ -0,0 +1,59 @@
+const mongoose= require('mongoose')
+
+if(process.argv.length<3){
+    console.log('give password atleast')
+    process.exit(1)
+}
+
+
+if(process.argv.length===4){
+    console.log('insuffcient number of arguments')
+    process.exit(1)
+}
+
+const password=process.argv[2]
+const url=`mongodb+srv://sayantan251204:${password}@phonebook.q2wk4.mongodb.net/person?retryWrites=true&w=majority&appName=phonebook`
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+
+
+const personSchema=new mongoose.Schema({
+    name:String,
+    number:Number,
+})
+
+const Person=mongoose.model('Person',personSchema)
+
+if(process.argv.length==5){
+
+    const person=new Person({
+        name:process.argv[3],
+        number:process.argv[4]
+    })
+    
+    person.save().then(res=>{
+        console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+
+else if(process.argv.length==3){
+    console.log("phonebook")
+    Person.find({}).then(res=>{
+        res.forEach(p=>{
+            console.log(`${p.name} ${p.number}`)
+        })
+        mongoose.connection.close()
+    })
+
+}
+
+
+
+
+
+
+
+
